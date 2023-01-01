21485b1e4278b9ec0c48b8c9a212fbd372ff7689
zedoraps <philip@moesli.net>
2023-01-01 19:48:14 +0100

    added material ui

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -14,34 +14,23 @@
         redirectUri = window.location.protocol + "//" + window.location.host + "/redirect"
     });
 </script>
-
-{#if $currentUser}
-    <p>👨‍💻 Iglogt als {$currentUser.username}</p>
-{:else if authMethods.length < 1}
-    Grad am lade 🏃‍♂️
-{:else}
-    <span>Ilogge met:</span>
-    {#each authMethods as prov (authMethods.id)}
-        <a href="{prov.authUrl + redirectUri}"
-           on:click={() => {localStorage.setItem('provider', JSON.stringify(prov))}}>
-                    <img class="google-icon"
-                         src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
-                <span class="btn-text"><b>Ilogge met Google</b></span>
-        </a>
-    {/each}
-{/if}
-
-
-<style>
-    a {
-        text-decoration: none;
-        color: white;
-        background-color: #212529;
-        display: flex;
-        gap: 1rem;
-        padding: 1rem;
-        border-radius: 2px;
-        margin: 0.3rem 1rem;
-    }
-
-</style>
+<div class="responsive">
+    {#if $currentUser}
+        <p>👨‍💻 Iglogt als {$currentUser.username}</p>
+    {:else if authMethods.length < 1}
+        <div style="margin: 0 auto; display: block; width: 100px; margin-top: 200px;">
+            <a class="loader large"></a>
+        </div>
+    {:else}
+        {#each authMethods as prov (authMethods.id)}
+                <a href="{prov.authUrl + redirectUri}"
+                   on:click={() => {localStorage.setItem('provider', JSON.stringify(prov))}} class="responsive large-margin">
+                    <button class="large medium-elevate responsive">
+                        <img class="responsive"
+                             src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"/>
+                        <span>Ilogge met Google</span>
+                    </button>
+                </a>
+        {/each}
+    {/if}
+</div>
